f518607dd0bd91b50ef39324176b5768cda01c15
Marius Tomek <marius.tomek@outlook.com>
2025-06-27 09:59:52 +0200

    Added special folder for BNE examples and added some new examples

---
+++ bindings/python/examples/BNE/BNE_train_and_save.py
@@ -0,0 +1,31 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BNE
+from tokenizers.trainers import BneTrainer
+from tokenizers.pre_tokenizers import ByteLevel, Whitespace
+import datasets
+
+
+# Build tokenizer
+tokenizer = Tokenizer(BNE(unk_token="[UNK]"))
+trainer = BneTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+tokenizer.pre_tokenizer = ByteLevel()
+
+# Load dataset
+dataset = datasets.load_dataset("wikitext", "wikitext-103-raw-v1", split="train")
+
+
+# Build an iterator over this dataset
+def batch_iterator():
+    batch_size = 1000
+    for batch in dataset.iter(batch_size=batch_size):
+        yield batch["text"]
+
+
+print(tokenizer.pre_tokenizer.pre_tokenize)
+
+tokenizer.train_from_iterator(batch_iterator(), trainer, length=len(dataset))
+tokenizer.save("data/bne-byte-level.json")
+
+trainer.save_model("data/", "bne_byte_wt103")
+
+# To implement: https://huggingface.co/docs/tokenizers/v0.20.3/en/api/models#tokenizers.models.Model
